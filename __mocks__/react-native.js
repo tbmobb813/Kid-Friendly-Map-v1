886d568aa0f1e84667f5020c83c015995f22d7d4
tbmobb813 <jsn.nix@gmail.com>
2025-10-04 06:41:54 -0400

    feat: Initialize Gradle wrapper and scripts for Android project
    - Added gradle-wrapper.properties to configure Gradle distribution.
    - Included gradlew and gradlew.bat scripts for executing Gradle tasks.
    - Created settings.gradle to manage plugin repositories and include builds.
    - Implemented MapWithInfoPanel component with interactive features and sound effects.
    - Defined types/index.ts to export navigation types.

--- __mocks__/react-native.js
+++ __mocks__/react-native.js
@@ -1,23 +1,7 @@
-const React = require('react');
-
-// Minimal mock of react-native for Jest in a Node environment
-const Platform = {
-  OS: 'linux',
-  Version: '1.0.0'
-};
-
-const View = ({ children, ...props }) => React.createElement('div', props, children);
-const Text = ({ children, ...props }) => React.createElement('span', props, children);
-const Pressable = ({ children, onPress, ...props }) => React.createElement('button', { onClick: onPress, ...props }, children);
-
+// Use actual react-native exports for Jest compatibility
+const rn = require('react-native');
 module.exports = {
-  Platform,
-  View,
-  Text,
-  Pressable,
-  // Provide defaults for other RN exports that may be referenced
-  StyleSheet: {
-    create: (styles) => styles,
-    flatten: (style) => style
-  }
+  ...rn,
+  StyleSheet: rn.StyleSheet,
+  Platform: { ...rn.Platform, OS: 'android' },
 };
