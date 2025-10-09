046db960a6715686f186968aa76714dbf1425880
tbmobb813 <84993209+tbmobb813@users.noreply.github.com>
2025-10-09 00:49:48 +0000

    chore: update package.json scripts and dependencies
    - Adjusted test scripts for consistency and readability.
    - Added @typescript-eslint/parser and metro-react-native-babel-preset dependencies.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function (api) {
+export default function (api) {
   api.cache(true);
   return {
     presets: [
@@ -11,4 +11,4 @@ module.exports = function (api) {
     ],
     plugins: ['react-native-reanimated/plugin'],
   };
-};
+}
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   globals: {
